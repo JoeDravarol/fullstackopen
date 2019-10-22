232047596bf7f1dd38197d9d303a0913fffb55ef
Joe <joedravarol@gmail.com>
2019-10-22 03:39:41 +0100

    Complete courseinfo step5
    Refactor App component course and its parts into a single
    JavaScript object.

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
+const Header = ({ course }) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{course.name}</h1>
     </>
   )
 }
@@ -17,7 +17,7 @@ const Part = (props) => {
   )
 }
 
-const Content = ({ parts }) => {
+const Content = ({ course: { parts } }) => {
   return (
     <>
       <Part part={parts[0].name} exercise={parts[0].exercises} />
@@ -27,7 +27,7 @@ const Content = ({ parts }) => {
   )
 }
 
-const Total = ({ parts }) => {
+const Total = ({ course: { parts } }) => {
   return (
     <>
       <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
@@ -36,27 +36,29 @@ const Total = ({ parts }) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
