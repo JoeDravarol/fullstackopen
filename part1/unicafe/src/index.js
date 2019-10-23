e10f1e755e518522a6e541a27a871ab9c71d9241
Joe <joedravarol@gmail.com>
2019-10-23 03:08:08 +0100

    Complete unicafe step 2 & 3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,7 +5,25 @@ const Header = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Statistics = ({ text, ratings }) => <p>{text} {ratings}</p>
+const Paragraph = ({ text, statistic }) => <p>{text} {statistic}</p>
+
+const Statistics = ({ good, neutral, bad }) => {
+  const sumAll = good + neutral + bad
+  const sumAverage = (good - bad) / sumAll
+  const sumPositive = (good / sumAll) * 100
+
+  return (
+    <>
+      <Header text='statistics' />
+      <Paragraph text='good' statistic={good} />
+      <Paragraph text='neutral' statistic={neutral} />
+      <Paragraph text='bad' statistic={bad} />
+      <Paragraph text='all' statistic={sumAll} />
+      <Paragraph text='average' statistic={sumAverage} />
+      <Paragraph text='positive' statistic={sumPositive + ' %'} />
+    </>
+  )
+}
 
 
 const App = () => {
@@ -14,22 +32,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(2)
   const [bad, setBad] = useState(1)
 
-  const setToValue = (value, setType) => () => setType(value) 
+  const setToValue = (value, setType) => () => setType(value)
 
   return (
     <div>
       <Header text='give feedback' />
-      <Button onClick={setToValue( (good + 1), setGood )} text='good' />
-      <Button onClick={setToValue( (neutral + 1), setNeutral)} text='neutral' />
-      <Button onClick={setToValue( (bad + 1), setBad)} text='bad' />
-      <Header text='statistics' />
-      <Statistics text='good' ratings={good} />
-      <Statistics text='neutral' ratings={neutral} />
-      <Statistics text='bad' ratings={bad} />
+      <Button onClick={setToValue((good + 1), setGood)} text='good' />
+      <Button onClick={setToValue((neutral + 1), setNeutral)} text='neutral' />
+      <Button onClick={setToValue((bad + 1), setBad)} text='bad' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
