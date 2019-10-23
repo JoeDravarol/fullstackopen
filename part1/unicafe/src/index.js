cd3645cd3c92cb4f21a1aba5f94365b04a65fea9
Joe <joedravarol@gmail.com>
2019-10-23 03:20:24 +0100

    Complete unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,7 +5,7 @@ const Header = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Paragraph = ({ text, statistic }) => <p>{text} {statistic}</p>
+const Statistic = ({ text, value }) => <p>{text} {value}</p>
 
 const Statistics = ({ good, neutral, bad }) => {
   const sumAll = good + neutral + bad
@@ -14,23 +14,21 @@ const Statistics = ({ good, neutral, bad }) => {
 
   if (good || neutral || bad) {
     return (
-      <>
-        <Header text='statistics' />
-        <Paragraph text='good' statistic={good} />
-        <Paragraph text='neutral' statistic={neutral} />
-        <Paragraph text='bad' statistic={bad} />
-        <Paragraph text='all' statistic={sumAll} />
-        <Paragraph text='average' statistic={sumAverage} />
-        <Paragraph text='positive' statistic={sumPositive + ' %'} />
-      </>
+      <div>
+        <Statistic text='good' value={good} />
+        <Statistic text='neutral' value={neutral} />
+        <Statistic text='bad' value={bad} />
+        <Statistic text='all' value={sumAll} />
+        <Statistic text='average' value={sumAverage} />
+        <Statistic text='positive' value={sumPositive + ' %'} />
+      </div>
     )
   }
 
   return (
-    <>
-      <Header text='statistics' />
-      <Paragraph text='No feedback given' />
-    </>
+    <div>
+      <Statistic text='No feedback given' />
+    </div>
   )
 }
 
@@ -49,6 +47,7 @@ const App = () => {
       <Button onClick={setToValue((good + 1), setGood)} text='good' />
       <Button onClick={setToValue((neutral + 1), setNeutral)} text='neutral' />
       <Button onClick={setToValue((bad + 1), setBad)} text='bad' />
+      <Header text='statistics' />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
