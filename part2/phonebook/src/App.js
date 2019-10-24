6cc4d9745654da353a1288ce1a42521887a639ef
Joe <joedravarol@gmail.com>
2019-10-24 23:43:34 +0100

    Complete phonebook step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,39 +5,51 @@ const App = () => {
     { name: 'Arto Hellas' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = () => {
-    const nameObject = { name: newName }
-
-    setPersons(persons.concat(nameObject))
-
+  const clearInputs = () => {
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleAddName = (event) => {
+  const addPerson = () => {
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons(persons.concat(personObject))
+    clearInputs()
+  }
+
+  const handleSubmission = (event) => {
     event.preventDefault()
 
     const doesNameExist = persons.find(person => person.name === newName) ? true : false;
 
     if (doesNameExist) {
       alert(`${newName} is already added to phonebook`)
-
-      setNewName('')
+      clearInputs()
     } else {
-      addName()
+      addPerson()
     }
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
 
-  const listPersons = () => persons.map(person => <li key={person.name}>{person.name}</li>)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+
+  const listPersons = () => persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAddName}>
+      <form onSubmit={handleSubmission}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          numbers: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
