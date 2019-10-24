12e8a0710912116a2527056343e75648fee31f53
Joe <joedravarol@gmail.com>
2019-10-25 00:16:51 +0100

    Complete phonebook step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showPerson, setShowPerson] = useState('')
 
   const clearInputs = () => {
     setNewName('')
@@ -19,7 +23,6 @@ const App = () => {
     }
 
     setPersons(persons.concat(personObject))
-    clearInputs()
   }
 
   const handleSubmission = (event) => {
@@ -29,21 +32,43 @@ const App = () => {
 
     if (doesNameExist) {
       alert(`${newName} is already added to phonebook`)
-      clearInputs()
     } else {
       addPerson()
     }
+
+    clearInputs()
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
 
   const handleNumberChange = (event) => setNewNumber(event.target.value)
 
-  const listPersons = () => persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
+  const handleFilterChange = (event) => setShowPerson(event.target.value)
+
+  const personsToShow = () => {
+    if (showPerson !== '') {
+      const filterList = persons.filter(person => {
+        const names = person.name.toLowerCase()
+        const filterValue = showPerson.toLowerCase()
+
+        return names.indexOf(filterValue) !== -1
+      })
+      
+      return filterList
+    }
+
+    return persons
+  }
+
+  const listPersons = () => personsToShow().map(person => <li key={person.name}>{person.name} {person.number}</li>)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={showPerson} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmission}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
