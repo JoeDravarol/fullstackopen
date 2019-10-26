bea68402588e79c1a8227770b4e217a69cce3988
Joe <joedravarol@gmail.com>
2019-10-26 23:49:31 +0100

    Complete countriesdata step 1

---
+++ part2/countriesdata/src/App.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+      </ul>
+      <img src={country.flag} alt={`${country.name}'s flag`} width='250px' />
+    </div>
+  )
+}
+
+const Countries = ({ filterCountries }) => {
+
+  if (filterCountries.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+
+  } else if (filterCountries.length > 1) {
+    return filterCountries.map(country => (
+      <div>
+        <p>{country.name}</p>
+      </div>
+    ))
+  }
+
+  const rows = filterCountries.map(country => <Country country={country} />)
+
+  return (
+    <>
+      {rows}
+    </>
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    const url = 'https://restcountries.eu/rest/v2/all'
+
+    axios
+      .get(url)
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const filterCountries = () => {
+    return countries.filter(country => country.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
+  }
+
+  const displayCountries = () => {
+    if (filter !== '') {
+      return <Countries filterCountries={filterCountries()} />
+    }
+  }
+
+  return (
+    <div>
+      find countries <input value={filter} onChange={handleFilterChange} />
+      {displayCountries()}
+    </div>
+  )
+}
+
+export default App;
