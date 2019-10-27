605fb198963c2bffc1cfd852a4a3f4691ef5911f
Joe <joedravarol@gmail.com>
2019-10-27 01:59:36 +0100

    Complete countriesdata step 3

--- part2/countriesdata/src/App.js
+++ part2/countriesdata/src/App.js
@@ -1,7 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import config from './config'
+
+const Weather = ({ lat, lng }) => {
+  const [weatherData, setWeatherData] = useState('')
+  const url = `${config.proxy}${config.url}${lat},${lng}`
+
+  const toCelcius = (fahrenheit) => {
+    return Math.round((fahrenheit - 32) * 5 / 9)
+  }
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(response => {
+        setWeatherData(response.data)
+      })
+  }, [])
+
+  if (weatherData !== '') {
+    const fahrenheit = weatherData.currently.temperature
+
+    return (
+      <>
+        <p><strong>temperature:</strong> {toCelcius(fahrenheit)} in Celcius</p>
+        <p><strong>wind:</strong> {weatherData.currently.windSpeed} mph</p>
+      </>
+    )
+  }
+  return (
+    <>
+    </>
+  )
+}
 
 const Country = ({ country }) => {
+  const latitude = country.latlng[0]
+  const longitude = country.latlng[1]
+
   return (
     <div>
       <h1>{country.name}</h1>
@@ -12,6 +48,8 @@ const Country = ({ country }) => {
         {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
       </ul>
       <img src={country.flag} alt={`${country.name}'s flag`} width='250px' />
+      <h2>Weather in {country.capital}</h2>
+      <Weather lat={latitude} lng={longitude} />
     </div>
   )
 }
