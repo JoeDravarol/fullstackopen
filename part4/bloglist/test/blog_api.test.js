188bd8ae0dfd2bd619937ae68fe8c84483d8d470
Joe <joedravarol@gmail.com>
2019-11-05 02:54:02 +0000

    Complete blog lists test step 3

--- part4/bloglist/test/blog_api.test.js
+++ part4/bloglist/test/blog_api.test.js
@@ -27,6 +27,29 @@ test('blog post unique identifier should be named id', async () => {
   expect(blogToView.id).toBeDefined()
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'The ways of integration testing',
+    author: 'Mii',
+    url: 'https://testingblog.com',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body.length).toBe(helper.initialBlogs.length + 1)
+  expect(titles).toContain(
+    'The ways of integration testing'
+  )
+})
+
 afterAll(() => {
   mongoose.connection.close()
 }) 
