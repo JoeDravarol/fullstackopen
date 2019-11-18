d0f261b6db107382491d310ee9d34b9307e5433b
Joe <joedravarol@gmail.com>
2019-11-18 19:56:41 +0000

    Adding new blogs require a valid token

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { blogs: 0 })
@@ -9,13 +18,23 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const blog = request.body.hasOwnProperty('likes')
-    ? new Blog(request.body)
-    : new Blog({ ...request.body, likes: 0 });
+  const body = request.body
 
-  const user = await User.findById(request.body.userId)
+  const blog = body.hasOwnProperty('likes')
+    ? new Blog(body)
+    : new Blog({ ...body, likes: 0 });
+
+  const token = getTokenFrom(request)
 
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
     if (request.body.hasOwnProperty('title') && request.body.hasOwnProperty('url')) {
       const savedBlog = await blog.save()
 
