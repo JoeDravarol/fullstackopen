17f922277ee3e8b14f930cf58c925ce126f76b52
Joe <joedravarol@gmail.com>
2019-12-02 01:16:25 +0000

    Complete exercise 5.6
    Make blog post display all of its information
    when the blog title is click and hide the
    information when the title is clicked again

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+ 
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const showWhenVisible = { display: visible ? '' : 'none' }
 
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+ 
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+ 
+  return (
+    <div style={blogStyle}>
+      <div>
+        <p onClick={toggleVisibility}>{blog.title} {blog.author}</p>
+        <a href={blog.url} style={showWhenVisible}>{blog.url}</a>
+        <div style={showWhenVisible}>
+          {blog.likes} likes
+            <button>like</button>
+        </div>
+        <p style={showWhenVisible}>
+          added by {blog.user.name}
+        </p>
+      </div>
+    </div>
+  )
+}
+ 
 export default Blog
