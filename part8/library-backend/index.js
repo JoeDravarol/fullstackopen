a01c2f44d1bb9ca30cd3ce2c2c5efff2f358ba91
Joe <joedravarol@gmail.com>
2019-12-20 01:56:00 +0000

    8.4: Books of an author (Completed)
    Modify the allBooks query so, that user can give an optional parameter
    author. The response should include only books written by that author.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -101,7 +101,9 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(
+      author: String
+    ): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -110,7 +112,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books
+      const byAuthor = (book) =>
+        book.author === args.author
+      return books.filter(byAuthor)
+    },
     allAuthors: () => authors
   },
   Author: {
