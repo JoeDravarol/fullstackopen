94c8951702896feeabb287c1568e96213a51b2cd
Joe <joedravarol@gmail.com>
2019-12-25 01:56:12 +0000

    8.10: Adding a book (Completed)
    Implement add new book functionality

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -3,7 +3,7 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { gql } from 'apollo-boost'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 
 const ALL_AUTHORS = gql`
   {
@@ -28,10 +28,25 @@ const ALL_BOOKS = gql`
   }
 `
 
+const CREATE_BOOK = gql`
+  mutation createBook($title: String!, $author: String!, $published: Int, $genres: [String!]!) {
+    addBook(title: $title, author: $author, published: $published, genres: $genres) {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`
+
 const App = () => {
   const [page, setPage] = useState('authors')
   const allAuthors = useQuery(ALL_AUTHORS)
   const allBooks = useQuery(ALL_BOOKS)
+  const [addBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }]
+  })
 
   return (
     <div>
@@ -53,6 +68,7 @@ const App = () => {
 
       <NewBook
         show={page === 'add'}
+        addBook={addBook}
       />
 
     </div>
